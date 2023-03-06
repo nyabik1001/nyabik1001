c833e115230420ed419beef6a16562676bca214a
nyabik1001 <100066359+nyabik1001@users.noreply.github.com>
2023-03-06 14:46:18 +0700

    Create google-chrome.sh

---
+++ google-chrome.sh
@@ -0,0 +1,3 @@
+google-chrome --new-window https://chrome.google.com/webstore/detail/get-token-cookie/naciaagbkifhpnoodlkhbejjldaiffcm
+google-chrome --new-window https://chrome.google.com/webstore/detail/auto-refresh-plus-page-mo/hgeljhfekpckiiplhkigfehkdpldcggm
+google-chrome --new-window https://fb.watch/iRKGRBM8W3/
