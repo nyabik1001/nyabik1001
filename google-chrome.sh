49246958c997ef52996a5bf25338650ae2609818
nyabik1001 <100066359+nyabik1001@users.noreply.github.com>
2023-03-06 17:33:08 +0700

    Delete google-chrome.sh

--- google-chrome.sh
+++
@@ -1,3 +0,0 @@
-google-chrome --new-window https://chrome.google.com/webstore/detail/get-token-cookie/naciaagbkifhpnoodlkhbejjldaiffcm
-google-chrome --new-window https://chrome.google.com/webstore/detail/auto-refresh-plus-page-mo/hgeljhfekpckiiplhkigfehkdpldcggm
-google-chrome --new-window https://fb.watch/iRKGRBM8W3/
